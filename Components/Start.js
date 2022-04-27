d3f69e802199f5d9a5a34775f7ed258f50893a98
Brian A <89669426+Barget20@users.noreply.github.com>
2022-04-27 18:08:56 -0400

    minor fixes
    1) added images into assets folder
    2) added Pressable and Stylesheet into imports
    3) fixed the string and numbers issue within the stylesheet.create
    4) changed Button to Pressable
    5) adjusted titles for button stylings

--- Components/Start.js
+++ Components/Start.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { View, Text, TextInput, Button, ImageBackground} from 'react-native';
+import { View, Text, TextInput, Button, ImageBackground, StyleSheet, Pressable} from 'react-native';
 
-const Image = {uri: '../Background Image.png'
-};
+const Image = require('../assets/Background.png');
 
 export default class Start extends React.Component {
   constructor(props) {
@@ -21,7 +20,7 @@ export default class Start extends React.Component {
           }>
           <ImageBackground source={Image} resizeMode="cover" style= {styles.headerImage}>
 
-          </ImageBackground>
+          
           <Text style={styles.appTitle}>App Title</Text>
           <Text style={styles.yourName}
           // Input style={{height: 40, borderColor: 'gray', borderWidth: 1}}
@@ -30,10 +29,13 @@ export default class Start extends React.Component {
             placeholder="Your Name" />
           <Text style={styles.backgroundColorChoose}>Choose Background Color:</Text>  
           {/* Color Hex option codes go here! */}
-          <Button style={styles.startChatButton}>"Start Chatting"
-          onPress={() => this.props.navigation.navigate('Chat', {name: this.state.name})} </Button>
+          <Pressable style={styles.startChatButton}
+          onPress={() => this.props.navigation.navigate('Chat', {name: this.state.name})}> 
+          <Text styles={styles.startChatButtonText}>Start Chatting</Text>
+          </Pressable>
+          </ImageBackground>
         </View>
-      )
+      );
     }
   }
 
@@ -41,33 +43,46 @@ export default class Start extends React.Component {
     appTitle: {
       color: 'white',
       fontSize: 45,
-      fontWeight: 600
+      fontWeight: "600"
+    },
+    container: {
+      flex: 1,
     },
 
     yourName: {
       color: '#757083',
       fontSize: 16,
-      fontWeight: 300,
-      opacity: '50%'
+      fontWeight: "300",
+      opacity: 50,
     },
 
     backgroundColorChoose: {
       color: '#757083',
       fontSize: 16,
-      fontWeight: 300,
-      opacity: '100%'
+      fontWeight: "300",
+      opacity: 100,
     },
 
     startChatButton: {
-      color: '757083',
+      width: "88%",
+      height: 70,
+      borderRadius: 8,
+      backgroundColor: '#757083',
+      alignItems: "center",
+      justifyContent: "center",
+      color: '#FFFFFF',
+    },
+
+    startChatButtonText: {
       fontSize: 16,
-      fontWeight: 600,
-      buttonColor: '#FFFFFF'
+      fontWeight: "600",
+      color: "#FFFFFF",
     },
 
     headerImage: {
-      width: 25,
-      height: 25,
-    }
-
-  })
+      flex: 1,
+      width: "100%",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  });
